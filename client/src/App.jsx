e945051c44752d7c469180689daa006b1b2e01ec
pruthvi099 <65605220+pruthvi099@users.noreply.github.com>
2021-10-06 08:29:09 +0530

    Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,12 @@
+
+//HOC
+import HomeLayoutHOC from "./HOC/Home.Hoc"
+import Temp from "./components/temp"
 function App() {
   return (
-    <div> </div>
+    <div> 
+    <HomeLayoutHOC path="/" exact component={Temp}/>
+    </div>
   );
 }
 
